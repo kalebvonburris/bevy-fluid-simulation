4a02bb2c33b4c29a304c4149274ffae4891f103a
Kaleb Burris <kaleb.r.burris@gmail.com>
2023-10-12 18:17:42 -0800

    A square! And version downgrading

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,26 @@
 // 10-12-2023
 // TODO: Write file description.
 
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
+    .add_systems(Startup, setup)
     .add_systems(Update, bevy::window::close_on_esc)
     .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+        transform: Transform::default().with_scale(Vec3::splat(128.)),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        ..default()
+    });
 }

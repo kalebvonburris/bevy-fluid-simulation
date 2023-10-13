b1c3a56aedbf91c203e9d72842fcd897c51ffac4
Kaleb Burris <kaleb.r.burris@gmail.com>
2023-10-12 23:10:10 -0800

    Logging added

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@
 mod particle;
 use particle::*;
 
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, diagnostic::{LogDiagnosticsPlugin, FrameTimeDiagnosticsPlugin}};
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
         .insert_resource(Msaa::Sample4)
         .add_plugins(DefaultPlugins)
+        .add_plugins(LogDiagnosticsPlugin::default())
+        .add_plugins(FrameTimeDiagnosticsPlugin::default())
         .init_resource::<Gravity>()
         .add_systems(Startup, setup)
         .add_systems(Update, bevy::window::close_on_esc)
@@ -31,17 +33,19 @@ fn setup(
     // PURPLE CIRCLES!!!
     for y in -50..50 {
         for x in -50..50 {
-            commands.spawn(
-                MaterialMesh2dBundle {
-                mesh: meshes.add(Mesh::from(shape::Circle::default())).into(),
-                material: materials.add(ColorMaterial::from(Color::BLUE)),
-                ..default()}
-            ).insert(ParticleBundle {
-                pos: Transform::from_xyz((x * 10) as f32, (y * 10) as f32, 0.0).with_scale(Vec3::splat(10.0)),
-                mass: Mass(1.0),
-                collider: CircleCollider::default(),
-                velocity: Velocity::new(0.0, -1.0)
-            });
+            commands
+                .spawn(MaterialMesh2dBundle {
+                    mesh: meshes.add(Mesh::from(shape::Circle::default())).into(),
+                    material: materials.add(ColorMaterial::from(Color::BLUE)),
+                    ..default()
+                })
+                .insert(ParticleBundle {
+                    pos: Transform::from_xyz((x * 10) as f32, (y * 10) as f32, 0.0)
+                        .with_scale(Vec3::splat(10.0)),
+                    mass: Mass(1.0),
+                    collider: CircleCollider::default(),
+                    velocity: Velocity::new(0.0, -9.8),
+                });
         }
     }
 }
--- src/particle.rs
+++ src/particle.rs
@@ -2,10 +2,12 @@
 // Kaleb Burris
 // 10-12-2023
 // The necessary components to simulate fluid dynamics using particles.
-const DELTA_TIME: f32 = 1. / 60.;
+const GRAVITY: f32 = -9.8;
 
 use bevy::{
-    prelude::{Component, Res, Vec2, Query, Mesh, shape, Transform, Vec3, Material, Resource, Bundle},
+    prelude::{
+        shape, Bundle, Component, Material, Mesh, Query, Res, Resource, Transform, Vec2, Vec3,
+    },
     time::Time,
 };
 
@@ -44,15 +46,20 @@ pub struct Velocity {
 }
 
 impl Velocity {
-    pub fn new(x: f32, y: f32) -> Self { Self { vec: Vec2::new(x, y) } }
+    pub fn new(x: f32, y: f32) -> Self {
+        Self {
+            vec: Vec2::new(x, y),
+        }
+    }
 }
 
 impl Default for Velocity {
     fn default() -> Self {
-        Self { vec: Vec2::new(0.0, 0.0) }
+        Self {
+            vec: Vec2::new(0.0, 0.0),
+        }
     }
 }
-
 
 #[derive(Default, Debug, Bundle)]
 pub struct ParticleBundle {
@@ -63,8 +70,9 @@ pub struct ParticleBundle {
 }
 
 /// Moves objects in the physics world
-pub fn simulate(mut query: Query<(&mut Transform, &Mass, &mut Velocity)>) {
-    for (mut pos, mass, velocity) in query.iter_mut() {
-        pos.translation.y += velocity.into_inner().vec[1] * DELTA_TIME;
+pub fn simulate(time: Res<Time>, mut query: Query<(&mut Transform, &Mass, &mut Velocity)>) {
+    for (mut pos, mass, mut velocity) in query.iter_mut() {
+        pos.translation.y += velocity.vec[1] * time.delta_seconds();
+        velocity.vec[1] += GRAVITY * time.delta_seconds();
     }
 }
